6818164034078645d313b6990fafd394963cdfdc
Osagras <148169604+Osagras@users.noreply.github.com>
2024-01-11 08:21:53 +0000

    counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,37 @@
 import React from "react";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClock} from '@fortawesome/free-regular-svg-icons'
+import {useState, useEffect} from "react"
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+
+	const [counter,setCounter] = useState(0);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setCounter(counter => counter + 1)
+		},1000)
+
+		return () => clearInterval(interval)
+	},[counter])
+
+		function calculateSeconds (aCounter, placeValue) {
+			return Math.floor(aCounter / placeValue) % 10
+		}
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	<div className="counterDisplay">
+		<div className="clockImg">
+			<FontAwesomeIcon icon={faClock} />
 		</div>
-	);
+		<div className="secondsThousands">{calculateSeconds(counter, 1000)}</div>
+		<div className="secondsHundreds">{calculateSeconds(counter, 100)}</div>
+		<div className="secondsTens">{calculateSeconds(counter, 10)}</div>
+		<div className="secondsOnes">{calculateSeconds(counter, 1)}</div>
+	</div>)
 };
 
 export default Home;
